cdc88bc1df14a9ec5084752f372f7c333d3dd9bc
HadassaAvimor <h0533185978@gmail.com>
2023-11-23 12:32:28 +0200

    add option to recapture picture

--- client/src/components/camera/Camera.js
+++ client/src/components/camera/Camera.js
@@ -1,39 +1,48 @@
-import React, { useState } from 'react';
-import Webcam from 'react-webcam';
-import '../../style/Camera.css';
-// import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonChecked'
-// const WebcamComponent = () => <Webcam />
-const videoConstraints = {
-  width: 400,
-  height: 400,
-  facingMode: 'user',
-}
-const Camera = () => {
-  const [picture, setPicture] = useState('')
-  const webcamRef = React.useRef(null)
-  const capture = React.useCallback(() => {
-    const pictureSrc = webcamRef.current.getScreenshot();
-    setPicture(pictureSrc);
-  })
+
+import Webcam from "react-webcam";
+import React, { useState, useRef, useCallback } from "react";
+
+function Camera() {
+  const webcamRef = useRef(null);
+  const [img, setImg] = useState(null);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImg(imageSrc);
+  }, [webcamRef]);
+
+  const videoConstraints = {
+    width: 390,
+    height: 390,
+    facingMode: "user",
+  };
+
   return (
-    <div className="webcamCapture">
-      <Webcam
-        audio={false}
-        height={videoConstraints.height}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width={videoConstraints.width}
-        videoConstraints={videoConstraints}
-      />
-      <button    
-      type='radio'     
-       className="webcamCapture__button"
-        onClick={capture}
-        fontSize="large"
->
-        </button>
+    <div className="Container">
+      {img === null ? (
+        <>
+          <Webcam
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+            audio={false}
+            height={500}
+            width={500}
+            ref={webcamRef}
+            mirrored={true}
+          />
+          <button onClick={capture}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-fill" viewBox="0 0 16 16">
+            <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+            <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0" />
+          </svg></button>
+        </>
+      ) : (
+        <>
+          <img src={img} alt="screenshot" />
+          <button onClick={() => setImg(null)}>Recapture</button>
+        </>
+      )}
     </div>
   );
-
 }
-export default Camera
+
+export default Camera;
